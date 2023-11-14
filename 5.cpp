38e90a1474ad4aea069ddff020e835c7c1a97f94
Adeel Javed <93484102+adeel-015@users.noreply.github.com>
2023-11-14 14:25:15 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 class MyClass{
     public:
-        MyClass(){
-            cout << "This is a constructor!" << endl;
-        }
+    MyClass(){
+        cout << "This is a constructor!" << endl;
+    }
         
-        ~MyClass(){
-            cout << "This is a destructor!" << endl;
-        }
+    ~MyClass(){
+        cout << "This is a destructor!" << endl;
+    }
 };
 
 int main(){
